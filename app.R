356bbd071e67f06b9dd6e7039f68fa0d38b723bf
Mwangi George <mwangigeorge648@gmail.com>
2023-12-27 16:56:02 +0300

    Commented the app.R code

--- app.R
+++ app.R
@@ -1,7 +1,9 @@
+# Required Libraries
 pacman::p_load(
   tidyverse, janitor, shiny, bs4Dash, DT, waiter, thematic, shinyWidgets, gt, highcharter, paletteer, ggsci, shinycssloaders
 )
 
+# A global style for buttons
 button_style <- style <- "color: #fff; background-color: #007FFF; border-color: #007FFF; width: 150px; max-width: 150px; height: 40px; border-radius: 20px; border: 2px solid #007FFF;"
 
 # toast options
@@ -12,8 +14,10 @@ toastOpts <- list(
   position = "topRight"
 )
 
+# Data used by the app
 sales_df <- as_tibble(data.table::fread("data/dairy_dataset.csv")) %>%
   clean_names() %>%
+  # Getting columns into correct data types 
   mutate(
     production_date = ymd(production_date),
     date = ymd(date), 
@@ -22,15 +26,16 @@ sales_df <- as_tibble(data.table::fread("data/dairy_dataset.csv")) %>%
   )
 
 
-# Modules
+# Importing modules --- these contains the backend logic
 modules <- str_c("modules/", list.files("modules/"))
 map(modules, ~ source(.x))
 
-# App structure files
+# Importing app structure files --- they contain the scripts needed to build the UI
 app_files <- str_c("app_structure/", list.files("app_structure/"))
 map(app_files, ~ source(.x))
 
 
+# Build the UI using imported files
 ui <- dashboardPage(
   preloader = list(html = tagList(spin_1(), "Loading Data..."), color = "#343a40"),
   header = header,
@@ -45,7 +50,11 @@ ui <- dashboardPage(
   scrollToTop = TRUE
 )
 
+
+# Build the backend logic
 server <- function(input, output, session) {
+  
+  # This renders the Developers info (on the top right of the UI)
   output$user <- renderUser({
     dashboardUser(
       name = "George Mwangi",
@@ -54,9 +63,8 @@ server <- function(input, output, session) {
       subtitle = a("Let's Connect on LinkedIn", href = "https://linkedin.com/in/georgemwangikenya", target = "_blank", style = "text-decoration: underline;")
     )
   })
-  a("AI & Data Associate", href = "https://github.com/mwangi-george", target = "_blank", style = "text-decoration: underline;")
 
-
+  # Dynamically custom selectors when locations changes
   observe({
     input$location
 
@@ -80,6 +88,7 @@ server <- function(input, output, session) {
   })
   
 
+  # This creates a reactive global data frame for creating the visualizations in the UI
   df <- reactive({
     sales_df %>%
       filter(
@@ -93,9 +102,20 @@ server <- function(input, output, session) {
       )
   })
 
+  # 
   observe({
-    df()
 
+    # changes to listen to 
+    list(
+      df(),
+      input$locations_4_qty_sold,
+      input$location, 
+      input$locations_4_shelf_life_qty_stocked, 
+      input$shelf_life_products,
+      input$locations_4_revenue_over_time
+    )
+
+    # Render the backend logic from imported modules (while observing changes above)
     summary_stats_hchart_server("summary_hchart_output", data = df())
     summary_stats_table_server("summary_table_output_2", data = df())
     quantity_sold_by_product_server("qty_sold_by_product", data = df())
@@ -106,32 +126,24 @@ server <- function(input, output, session) {
     shelf_life_analysis_over_time_server("shelf_life_over_time", data = df())
     shelf_life_valueboxes_server("shelf_life_metrics", data = df())
     revenue_valueboxes_server("revenue_metrics", data = df())
-  })
-
-  observe({
-    list(df(), input$locations_4_qty_sold, input$location)
-
     qty_sold_by_product_sales_channel_location_server("qty_litres_kg_location", data = df(), location_to_plot = input$locations_4_qty_sold)
-  })
-  
-  observe({
-    list(df(), input$locations_4_shelf_life_qty_stocked, input$shelf_life_products, input$location)
-    
     shelf_life_and_qty_in_stock_server("rship_btw_shelf_qty_stocked", data = df(), location_to_plot = input$locations_4_shelf_life_qty_stocked)
-  })
-
-  observe({
-    list(df(), input$locations_4_revenue_over_time, input$location)
-    
     revenue_over_time_server("revenue_over_time", data = df(), location_to_plot = input$locations_4_revenue_over_time)
+
   })
 
-  
 
+  # Show or hide controlbar dynamically when its toggle button is clicked
   observeEvent(input$controlbarToggle, {
     updateControlbar(id = "controlbar")
   })
+  
+  # Show or hide left sidebar dynamically when its toggle button is clicked
+  observeEvent(input$sidebarToggle, {
+    updateSidebar(id = "sidebar")
+  })
 
+  # Update Custom sidebars upon changes in respective inputs
   observeEvent(input$toggle_card_sidebar, {
     updateBoxSidebar("locations_sidebar")
   })
@@ -144,6 +156,7 @@ server <- function(input, output, session) {
     updateBoxSidebar("locations_sidebar_shelf_life")
   })
   
+  # Show a message when user changes between light and dark modes
   observeEvent(input$dark_mode, {
     toast(
       title = if (input$dark_mode) "Dark theme on!" else "Light theme on",
@@ -151,10 +164,8 @@ server <- function(input, output, session) {
     )
   })
   
-  observeEvent(input$sidebarToggle, {
-    updateSidebar(id = "sidebar")
-  })
   
 }
 
+# initialize the app
 shinyApp(ui, server)
